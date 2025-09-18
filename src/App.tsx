265b3b1bf61d5389afe3527c2920d340ca19f4b5
Rodrigo Sardinha <rodrigosardinha@gmail.com>
2025-09-18 09:42:16 -0300

    feat: restructure app with new styling and components
    - Replace default Vite template with custom Heading component
    - Add new theme and global CSS files
    - Remove unused default styles and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Heading } from './components/Heading.tsx';
 
-function App() {
-  const [count, setCount] = useState(0);
+import './styles/theme.css';
+import './styles/global.css';
 
+export function App() {
+  console.log('Passou');
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
+        alias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
+        alias.
       </p>
     </>
   );
 }
-
-export default App;
