b9953bb199f3c998ba132d7003e0596be3fbdfc2
Rodrigo Sardinha <rodrigosardinha@gmail.com>
2025-09-24 09:34:09 -0300

    feat(container): add container component with styles and update app layout
    Add new Container component with modular CSS and move container styles from global.css to module.
    Update App.tsx to use new Container component for better layout structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import { Heading } from './components/Heading.tsx';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Conteiner';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo 1{' '}
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
-        alias.
-      </p>
+      <Container>
+        <Heading>Testanto Heading</Heading>
+        <section>LOGO</section>
+      </Container>
+      <Container>
+        <section>MENU</section>
+      </Container>
     </>
   );
 }
