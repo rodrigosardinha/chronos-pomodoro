f7528a57fdfc6815503632bbbb31d3d1dbdedf27
Rodrigo Sardinha <rodrigosardinha@gmail.com>
2025-09-24 09:06:26 -0300

    feat(ui): add lucide-react icons and update theme colors
    - Add lucide-react dependency for icon support
    - Refactor Heading component to be more reusable with children prop
    - Update global styles and theme colors for better consistency
    - Remove console.log from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { Heading } from './components/Heading.tsx';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-  console.log('Passou');
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
-        alias.
-      </p>
+      <Heading>
+        Olá Mundo 1{' '}
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
         alias.
